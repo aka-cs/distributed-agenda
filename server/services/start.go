91ee0f55eb8844335553d21039e14e9b03ff95d5
Javier Rodríguez Rodríguez <javier.rdguezr@gmail.com>
2022-07-03 14:04:02 -0400

    better rsa keys handling

--- server/services/start.go
+++ server/services/start.go
@@ -7,11 +7,18 @@ import (
 )
 
 var (
-	node *chord.Node
+	node       *chord.Node
+	rsaPrivate string
+	rsaPublic  string
 )
 
 func Start(rsaPrivateKeyPath string, rsaPublicteKeyPath string, network string) {
-	node, err := chord.DefaultNode("50050")
+	var err error
+
+	rsaPrivate = rsaPrivateKeyPath
+	rsaPublic = rsaPublicteKeyPath
+
+	node, err = chord.DefaultNode("50050")
 
 	if err != nil {
 		log.Fatalf("Can't start chord node")
@@ -25,7 +32,7 @@ func Start(rsaPrivateKeyPath string, rsaPublicteKeyPath string, network string) 
 
 	go StartGroupService(network, "0.0.0.0:50052")
 	go StartEventService(network, "0.0.0.0:50053")
-	go StartAuthServer(rsaPrivateKeyPath, network, "0.0.0.0:50054")
+	go StartAuthServer(network, "0.0.0.0:50054")
 	go StartHistoryService(network, "0.0.0.0:50055")
 	go StartUserService(network, "0.0.0.0:50051")
 }
